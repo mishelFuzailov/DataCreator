5bdaa1dadc48b3cf9be8686c5b27ce81fc9b49d4
mishelFuzailov <46195241+mishelFuzailov@users.noreply.github.com>
2021-12-09 15:17:53 +0200

    DONE

---
+++ packages/Gematria.1.1.0/src/Properties/AssemblyInfo.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("EllisWeb.Gematria")]
+[assembly: AssemblyDescription("A .net class library for calculating the Gematria value of strings of Hebrew text, or convert numbers into Hebrew Text")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Ellis Web Development")]
+[assembly: AssemblyProduct("EllisWeb.Gematria")]
+[assembly: AssemblyCopyright("Copyright © 2014")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6555a784-6aae-4a25-9092-d2c290328301")]
+[assembly: AssemblyVersion("1.0.*")]
